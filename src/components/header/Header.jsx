026446f0e4c3f4795ac24ac18b73e30a560dca93
Razieh Brz <barzegarrazieh895@gmail.com>
2025-06-09 17:14:22 +0330

    feat: add responsive header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import introImg from "../../assets/images/intro.jpg";
+
+function Header() {
+  return (
+    <header className="header pt-32">
+      <div className="container px-4 md:px-24">
+        <div className="flex flex-row items-center gap-10 mb-10">
+          <div className="md:basis-1/2 mb-5 hidden md:block transition-transform hover:scale-95 duration-500">
+            <img
+              src={introImg}
+              alt="Website introduction banner"
+              className="w-full rounded"
+            />
+          </div>
+          <div className="md:basis-1/2 flex flex-col gap-8 items-center">
+            <p className="text-3xl font-medium text-center">
+              Every story deserves to be heard. <br />
+              Publish your articles, share your expertise, and make an impact!
+              <br />
+            </p>
+            <Link
+              to={"/register"}
+              className="text-lg text-primary text-center py-1 px-3 rounded font-semibold bg-secondary hover:text-secondary hover:bg-white transition-colors duration-500"
+            >
+              Sign up & start writing!
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
