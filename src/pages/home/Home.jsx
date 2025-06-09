2becdfa63c533cf6d09727754daf67658d132071
Razieh Brz <barzegarrazieh895@gmail.com>
2025-06-09 19:44:27 +0330

    feat: add home page with article list fetching

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,53 @@
+import { Link } from "react-router-dom";
+import Layout from "../../components/layout/Layout";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Article from "../../components/article/Article";
+import Header from "../../components/header/Header";
+
+function Home() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/articles");
+        setArticles(response.data);
+      } catch (error) {
+        console.error("Error fetching articles:", error);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
+  return (
+    <>
+      <Layout>
+        <Header />
+        <div className="articles" id="articles">
+          <div className="container">
+            <div className="flex flex-row items-center flex-wrap gap-y-6">
+              {articles.map((article) => (
+                <Link
+                  key={article.id}
+                  to={`/articles/${article.id}`}
+                  className="md:basis-1/2 lg:basis-1/3 xl:basis-1/4 p-4 "
+                >
+                  <Article
+                    imgUrl={article.imgUrl}
+                    title={article.title}
+                    author={article.author}
+                    date={article.date}
+                    readingTime={article.readingTime}
+                  />
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+}
+
+export default Home;
