3d6b2f3a7365ab8893fc5370b3dd95f23bf8fe38
Razieh Brz <barzegarrazieh895@gmail.com>
2025-06-09 19:55:26 +0330

    feat: add article page with content fetching

---
+++ src/pages/articlePage/ArticlePage.jsx
@@ -0,0 +1,57 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import Layout from "../../components/layout/Layout";
+
+function ArticlePage() {
+  const [article, setArticle] = useState({});
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchArticle = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8000/articles/${params.id}`
+        );
+        setArticle(response.data);
+      } catch (error) {
+        console.error("Error fetching article:", error);
+      }
+
+      fetchArticle();
+    };
+  }, []);
+  return (
+    <>
+      <Layout>
+        <div className="article pt-32">
+          <div className="container flex justify-center">
+            <div className="flex flex-col w-9/12">
+              <div className="titleWrapper">
+                <h1 className="font-semibold text-3xl py-2 ">
+                  {article.title}
+                </h1>
+              </div>
+
+              <div className="info flex gap-x-3 mb-4">
+                <span className="text-gray-600">{article.author} </span>
+                <span className="text-gray-600">{article.date} </span>
+                <span className="text-gray-600">
+                  {article.readingTime} minutes
+                </span>
+              </div>
+
+              <img src={article.imgUrl} className="article w-full" alt="" />
+
+              <div className="content mt-4 text-lg ">
+                <p className="text-justify">{article.content}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+}
+
+export default ArticlePage;
