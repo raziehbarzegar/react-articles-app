04981615e40b66ed96c0ab6c4018801f3568f0a3
Razieh Brz <barzegarrazieh895@gmail.com>
2025-06-09 19:28:22 +0330

    feat: add layout component for consistent page structure

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../navbar/Navbar";
+import Footer from "../footer/Footer";
+
+function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
