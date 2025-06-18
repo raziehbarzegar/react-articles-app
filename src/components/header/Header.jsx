c42d2ee29ad0921d3520fcfce59ed7d9efa69bfc
razieh barzegar <razieh.brz.dev@gmail.com>
2025-06-18 16:03:20 +0330

    feat(ui): improve UI in Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,14 +6,14 @@ function Header() {
     <header className="header pt-32">
       <div className="container px-4 md:px-24">
         <div className="flex flex-row items-center gap-10 mb-10">
-          <div className="md:basis-1/2 mb-5 hidden md:block transition-transform hover:scale-95 duration-500">
+          <div className="lg:basis-1/2 mb-5 hidden lg:block transition-transform hover:scale-95 duration-500">
             <img
               src={introImg}
               alt="Website introduction banner"
               className="w-full rounded"
             />
           </div>
-          <div className="md:basis-1/2 flex flex-col gap-8 items-center">
+          <div className="lg:basis-1/2 flex flex-col gap-8 items-center">
             <p className="text-3xl font-medium text-center">
               Every story deserves to be heard. <br />
               Publish your articles, share your expertise, and make an impact!
