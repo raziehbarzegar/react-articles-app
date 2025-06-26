eaeaa0de914fc343acfa56e055f83cbdd7f03639
razieh barzegar <razieh.brz.dev@gmail.com>
2025-06-26 10:06:22 +0330

    feat(home): show limited articles with link to full articles page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -20,18 +20,27 @@ function Home() {
   return (
     <>
       <Header />
-      <div className="articles" id="articles">
+      <div className="articles">
         <Container>
           <div className="flex flex-row items-center flex-wrap gap-y-6">
-            {articles.map((article) => (
+            {articles.slice(0, 4).map((article) => (
               <Link
                 key={article.id}
                 to={`/articles/${article.id}`}
-                className="md:basis-1/2 lg:basis-1/3 p-4"
+                className="md:basis-1/2 lg:basis-1/3 xl:basis-1/4 p-4"
               >
                 <Article {...article} />
               </Link>
             ))}
+          </div>
+
+          <div className="flex justify-center mt-5">
+            <Link
+              to={"/articles"}
+              className="text-lg text-primary text-center py-1 px-3 rounded font-semibold bg-secondary hover:text-secondary hover:bg-white transition-colors duration-500"
+            >
+              Discover more stories
+            </Link>
           </div>
         </Container>
       </div>
